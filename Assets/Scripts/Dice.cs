7883111dea53afce7edbb8f2f1065a5974088b47
options3224@gmail.com <options3224@gmail.com>
2022-07-23 16:20:35 +0900

    두 주사위의 눈금이 다르거나, 눈금이 최대치(7)이면 합성하지 않음

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -7,6 +7,7 @@ public class Dice : MonoBehaviour {
     [SerializeField] private GameObject levelText;
     private float attackTime = 0.0f;    // 공격 주기(초)
     private Vector3 currentPosition;    // 주사위의 현재 좌표(드래그 후 원위치)
+    private Collider2D colliderDice;
     [SerializeField] private int level = 1;
 
     private bool isTrigger = false;
@@ -43,40 +44,17 @@ public class Dice : MonoBehaviour {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // 주사위 합성
     private void OnTriggerStay2D(Collider2D other) {
+        colliderDice = other;
         isTrigger = true;
-        if (other.tag == "Dice" && !Input.GetMouseButton(0)) {
-            DiceManager diceManagerScript = diceManager.GetComponent<DiceManager>();
-
-            // 합성할 두 주사위의 배열 인덱스 구하기
-            int gameObjectIndex = Array.IndexOf(diceManagerScript.diceArray, gameObject);
-            int otherGameObjectIndex = Array.IndexOf(diceManagerScript.diceArray, other.gameObject);
-
-            // 배열에서 주사위 제거(null 값으로 변경)
-            try {
-                diceManagerScript.diceArray[gameObjectIndex] = null;
-                diceManagerScript.diceArray[otherGameObjectIndex] = null;
-            } catch(IndexOutOfRangeException e) {
-                isTrigger = false;
-                return;
-            }
-
-            // 주사위 오브젝트 제거
-            Destroy(gameObject);
-            Destroy(other.gameObject);
-            // 주사위 LevelText 오브젝트 제거
-            Destroy(GetComponent<Dice>().levelText);
-            Destroy(other.GetComponent<Dice>().levelText);
-
-            // 새로운 주사위 1개 생성
-            diceManagerScript.createDice(otherGameObjectIndex, level+1);
-        }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
+        colliderDice = null;
         isTrigger = false;
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private void OnMouseDown() {
         // 원위치할 좌표 정보를 임시로 저장
@@ -93,12 +71,57 @@ public class Dice : MonoBehaviour {
     }
 
     private void OnMouseUp() {
-        if (!isTrigger) {   // 주사위 합성 트리거가 실행 중이 아닐 경우
-            // 주사위 원위치
-            transform.position = currentPosition;
-            // 레이어 순서 원래대로
-            GetComponent<SpriteRenderer>().sortingOrder -= 1;
-            tag = "Dice";
+        // 주사위의 위치가 겹치면 주사위 합성 수행
+        if (isTrigger) 
+            combine(colliderDice);
+        // 주사위 원위치
+        transform.position = currentPosition;
+        // 레이어 순서 원래대로
+        GetComponent<SpriteRenderer>().sortingOrder -= 1;
+        tag = "Dice";
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // 주사위 합성
+    private void combine(Collider2D other) {
+        if (other.tag != "Dice")
+            return;
+
+        DiceManager diceManagerScript = diceManager.GetComponent<DiceManager>();
+
+        // 합성할 두 주사위의 배열 인덱스 구하기
+        int gameObjectIndex = Array.IndexOf(diceManagerScript.diceArray, gameObject);
+        int otherGameObjectIndex = Array.IndexOf(diceManagerScript.diceArray, other.gameObject);
+
+        // 두 주사위의 눈금이 다르거나, 눈금이 최대치(7)일 경우 합성 없이 리턴
+        if (level != other.GetComponent<Dice>().level || level >= 7)
+            return;
+
+        // 배열에서 주사위 제거(null 값으로 변경)
+        try {
+            diceManagerScript.diceArray[gameObjectIndex] = null;
+            diceManagerScript.diceArray[otherGameObjectIndex] = null;
+        } catch(IndexOutOfRangeException e) {
+            isTrigger = false;
+            return;
         }
+
+        // 주사위 오브젝트 제거
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+        // 주사위 LevelText 오브젝트 제거
+        Destroy(GetComponent<Dice>().levelText);
+        Destroy(other.GetComponent<Dice>().levelText);
+
+        // 새로운 주사위 1개 생성
+        diceManagerScript.createDice(otherGameObjectIndex, level+1);
     }
 }
+
+
+
+
+
+
+
