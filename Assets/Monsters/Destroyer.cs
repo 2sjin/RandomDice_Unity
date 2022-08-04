80e362ff40bdbdc0eabc8193dc6df31892eec5fe
options3224@gmail.com <options3224@gmail.com>
2022-08-04 22:44:22 +0900

    플레이어의 남은 생명 수에 따른 하트 출력 반영

--- Assets/Monsters/Destroyer.cs
+++ Assets/Monsters/Destroyer.cs
@@ -9,7 +9,7 @@ public class Destroyer : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Monster") {
             other.GetComponent<Monster>().hp = 0;
-            player.GetComponent<Player>().heart -= 1;   // 하트 1 감소
+            player.GetComponent<Player>().life -= 1;   // 하트 1 감소
         }
         if (other.tag == "Skill")
             Destroy(other.gameObject);
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,23 +5,49 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour {    
     public int sp;
     public int spCost;
-    public int heart;
+    public int life;
     
     public GameObject spText;
     public GameObject spCostText;
+    public GameObject [] heart = new GameObject[3];
 
     private void Start() {
         this.sp = 100;
         this.spCost = 10;
-        this.heart = 3;
+        this.life = 3;
     }
 
     private void Update() {
+        // Sp, SpCost 텍스트 갱신
         spText.GetComponent<TextMesh>().text = sp.ToString();
         spCostText.GetComponent<TextMesh>().text = spCost.ToString();
-        
+
+        // 하트 이미지 갱신
+        switch (life) {
+            case 0:
+                heart[0].SetActive(false);
+                heart[1].SetActive(false);
+                heart[2].SetActive(false);
+                break;
+            case 1:
+                heart[0].SetActive(true);
+                heart[1].SetActive(false);
+                heart[2].SetActive(false);
+                break;
+            case 2:
+                heart[0].SetActive(true);
+                heart[1].SetActive(true);
+                heart[2].SetActive(false);
+                break;
+            default:
+                heart[0].SetActive(true);
+                heart[1].SetActive(true);
+                heart[2].SetActive(true);
+                break;
+        }
+
         // 게임 오버 체크
-        if (heart <= 0)
+        if (life <= 0)
             SceneManager.LoadScene("LobbyScene");
     }
 }
