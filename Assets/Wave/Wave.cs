7777098874d907a758afbea0291c0822238b98d6
options3224@gmail.com <options3224@gmail.com>
2022-08-05 13:01:11 +0900

    타이머 및 타이머 Text 1차 적용

--- Assets/Wave/Wave.cs
+++ Assets/Wave/Wave.cs
@@ -4,11 +4,26 @@ public class Wave : MonoBehaviour {
     public int wave;
     public GameObject waveText;
 
+    public int timer;
+    public float deltaTimer;
+    public GameObject timerText;
+
     void Start() {
         wave = 1;
+        deltaTimer = 0;
+        timer = 90;
     }
 
     void Update() {
         waveText.GetComponent<TextMesh>().text = wave.ToString();
+        timerText.GetComponent<TextMesh>().text = timer.ToString();
+
+        // deltaTime 1초마다 타이머 1씩 감소
+        deltaTimer += Time.deltaTime;
+        if (deltaTimer >= 1.0f) {
+            timer -= 1;
+            deltaTimer -= 1.0f;
+        }
+
     }
 }
