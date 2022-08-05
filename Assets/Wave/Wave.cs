5761d2b8fef5d3ff6c1b22bc796c1c8bd2e8a67f
options3224@gmail.com <options3224@gmail.com>
2022-08-05 12:54:27 +0900

    wave 값에 따라 waveText가 변경됨

--- Assets/Wave/Wave.cs
+++ Assets/Wave/Wave.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class Wave : MonoBehaviour {
     public int wave;
+    public GameObject waveText;
 
     void Start() {
         wave = 1;
     }
 
     void Update() {
-        
+        waveText.GetComponent<TextMesh>().text = wave.ToString();
     }
 }
