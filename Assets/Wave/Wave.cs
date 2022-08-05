c4bea4d2ce54b054b83f68f360abc69c1dc9b0eb
options3224@gmail.com <options3224@gmail.com>
2022-08-05 13:12:43 +0900

    타이머 mm:ss 단위로 출력, 타이머 음수 방지

--- Assets/Wave/Wave.cs
+++ Assets/Wave/Wave.cs
@@ -4,9 +4,10 @@ public class Wave : MonoBehaviour {
     public int wave;
     public GameObject waveText;
 
-    public int timer;
-    public float deltaTimer;
+    public int timer;               // 웨이브 메인 타이머
     public GameObject timerText;
+
+    private float deltaTimer;        // deltaTime으로 1초 단위를 측정하는 타이머
 
     void Start() {
         wave = 1;
@@ -15,8 +16,11 @@ public class Wave : MonoBehaviour {
     }
 
     void Update() {
+        // 현재 웨이브를 텍스트에 출력
         waveText.GetComponent<TextMesh>().text = wave.ToString();
-        timerText.GetComponent<TextMesh>().text = timer.ToString();
+
+        // 타이머의 시간을 텍스트에 출력(mm:ss)
+        timerText.GetComponent<TextMesh>().text = Mathf.FloorToInt(timer / 60).ToString("D2") + ":" + (timer % 60).ToString("D2");
 
         // deltaTime 1초마다 타이머 1씩 감소
         deltaTimer += Time.deltaTime;
@@ -24,6 +28,10 @@ public class Wave : MonoBehaviour {
             timer -= 1;
             deltaTimer -= 1.0f;
         }
+
+        // 타이머 음수 방지
+        if (timer < 0)
+            timer = 0;
 
     }
 }
